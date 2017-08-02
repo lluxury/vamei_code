7755d33fc34404a8660332f61c34b7b0c6c97811
yann <tmp4321@qq.com>
2017-08-02 10:21:22 +0800

    network socket py3 utf-8

---
+++ kernel_2/network/tsTclnt3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from socket import *
+
+HOST = '127.0.0.1'
+PORT = 21567
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpCliSock = socket(AF_INET, SOCK_STREAM)
+tcpCliSock.connect(ADDR)
+
+while True:
+    data = input('> ')
+    if not data:
+        break
+    #tcpCliSock.send(data)
+    tcpCliSock.send(bytes(data, 'utf-8'))
+    data = tcpCliSock.recv(BUFSIZ)
+    if not data:
+        break
+    print(data.decode('utf-8'))
+
+tcpCliSock.close()
+
+# 发送的同时需要说明字符集,注意 .send(bytes(data, 'utf-8'))
