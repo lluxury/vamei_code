cc2370782a5c8b6e1913d226a1a5301c9fecea94
yann <tmp4321@qq.com>
2017-08-02 10:20:48 +0800

    network socket

---
+++ kernel_2/network/tsTclnt.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from socket import *
+HOST = 'localhost'
+PORT = 21567
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpCliSock = socket(AF_INET, SOCK_STREAM)
+tcpCliSock.connect(ADDR)
+
+while True:
+    data = raw_input('> ')
+    if not data:
+        break
+    tcpCliSock.send(data)
+    data = tcpCliSock.recv(BUFSIZ)
+    if not data:
+        break
+    print data
+
+tcpCliSock.close()
+
+# cli发送data,再cli.recv发回来的buff,并打印
