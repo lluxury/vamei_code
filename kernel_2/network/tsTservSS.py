0c5deed498728e7279889841395dda15a2eca7cb
yann <tmp4321@qq.com>
2017-08-02 10:23:59 +0800

    network SocketServer class

---
+++ kernel_2/network/tsTservSS.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import SocketServer
+from time import ctime
+
+HOST = ''
+PORT = 21567
+ADDR = (HOST, PORT)
+
+class MyRequestHandler(SocketServer.StreamRequestHandler):
+    def handle(self):
+        print '...connected form:', self.client_address
+        #self.wfile.write('[%s] %s\n' % (ctime(), self.refile.readline().strip()))
+        self.wfile.write('[%s] %s\n' % (ctime(), self.rfile.readline().strip()))
+
+tcpSerSock = SocketServer.TCPServer(ADDR, MyRequestHandler)
+print 'waiting for connection...'
+tcpSerSock.serve_forever()
